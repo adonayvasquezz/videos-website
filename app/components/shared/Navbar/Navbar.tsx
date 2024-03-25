6bde738830ecd93098dbb1ce8dfd1d799f69bb81
Adonay Vasquez <adonay16.vz@gmail.com>
2024-03-25 08:46:20 -0600

    style: navbar responsive

--- app/components/shared/Navbar/Navbar.tsx
+++ app/components/shared/Navbar/Navbar.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import { SearchBar } from "..";
 
 export const Navbar = () => {
   return (
     <nav>
-      <Link href={"/"}>
-        <div className="mt-6 text-[1.65rem] text-blue-400">VIDEOS WEB</div>
-      </Link>
+      <div className="flex align-middle content-center items-center md:justify-between mt-4 md:mt-6 flex-col md:flex-row ">
+        <div className="text-[1.65rem] text-blue-400 w-full text-center md:text-left md:w-1/4">
+          <Link href={"/"}>VIDEOS WEB </Link>
+        </div>
+
+        <SearchBar />
+        <div className="w-1/4" />
+      </div>
     </nav>
   );
 };
---
+++ app/components/shared/SearchBar/SearchBar.tsx
@@ -0,0 +1,7 @@
+export const SearchBar = () => {
+  return (
+    <div className="bg-slate-400 w-3/4 md:w-1/3 mt-2">
+      <input className="w-full px-2" placeholder="Buscar videos" type="text" />
+    </div>
+  );
+};
