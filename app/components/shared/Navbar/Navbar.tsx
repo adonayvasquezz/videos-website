ee100e38219efce25107e24cf1329a99a4b9f8ce
Adonay Vasquez <adonay16.vz@gmail.com>
2024-03-24 20:23:07 -0600

    style: navbar style updated and redirection to home

--- app/components/shared/Navbar/Navbar.tsx
+++ app/components/shared/Navbar/Navbar.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export const Navbar = () => {
-  return <nav>navbar</nav>;
+  return (
+    <nav>
+      <Link href={"/"}>
+        <div className="mt-6 text-[1.65rem] text-blue-400">VIDEOS WEB</div>
+      </Link>
+    </nav>
+  );
 };
