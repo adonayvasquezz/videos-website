e65d02c06bb36a127ac2176b6758d8f0f024c2e5
Adonay Vasquez <adonay16.vz@gmail.com>
2024-03-25 08:47:11 -0600

    feat: title added to video detail

--- app/video/[slug]/page.tsx
+++ app/video/[slug]/page.tsx
@@ -3,6 +3,9 @@ import { EMBED_BASE_URL } from "@/app/lib/constants";
 import { home_videos } from "@/app/lib/placeholder-data";
 
 export default function Page({ params }: { params: { slug: string } }) {
+  // ToDo: detail endpoint call to get full video information
+
+  const title = "Claude 3 vs ChatGPT: ¿Quien lidera la IA? Mi veredicto";
   return (
     <>
       <div className="block mt-8 w-full md:w-[65%] h-[18rem] md:h-[65vh]">
@@ -12,6 +15,7 @@ export default function Page({ params }: { params: { slug: string } }) {
           allowFullScreen
         />
       </div>
+      <h1 className="mt-3 px-2 text-2xl">{title}</h1>
 
       <RelatedVideos videos={home_videos} />
     </>
