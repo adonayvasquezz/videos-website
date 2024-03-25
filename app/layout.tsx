e2b330d89363fa64e33cd109510c6d29152906d4
Adonay Vasquez <adonay16.vz@gmail.com>
2024-03-24 20:24:02 -0600

    style: horizontal padding in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} md:px-[5vw]`}>
         <Navbar />
         {children}
         <Footer />
