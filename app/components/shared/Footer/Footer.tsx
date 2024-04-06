f72b83b52baba93ec7eeff4f83dad2f73d057c26
Adonay Vasquez <adonay16.vz@gmail.com>
2024-04-06 11:55:17 -0600

    feat: footer added

--- app/components/shared/Footer/Footer.tsx
+++ app/components/shared/Footer/Footer.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="pt-8 pb-12 border-t-2 border-gray-600 mt-8">
+      <div className="flex justify-center">
+        <Link className="hover:text-blue-400 px-4" href={"/"}>
+          Anunciarse
+        </Link>
+        -
+        <Link className="hover:text-blue-400 px-4" href={"/"}>
+          Mapa del sitio
+        </Link>
+        -
+        <Link className="hover:text-blue-400 px-4" href={"/"}>
+          Términos y condiciones
+        </Link>
+        -
+        <Link className="hover:text-blue-400 px-4" href={"/"}>
+          Política de privacidad
+        </Link>
+      </div>
+    </div>
+  );
 };
